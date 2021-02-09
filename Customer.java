695a9f2acc2a214860fba770af0529ae1bda9d99
David <david.demo@federation.edu.au>
2021-02-09 19:25:25 +1100

    Added the basic version of Customer class (in Customer.java)

---
+++ Customer.java
@@ -0,0 +1,14 @@
+// This is the Customer class
+// @author: David
+
+public class Customer
+{
+	String name;
+
+	/** Create a new customer, with the specified name */
+	public Customer(String _name)
+	{
+		this.name = name;
+	}
+}
+
