a33e0851c24add1189c43bef72be2a8aef086601
Sophie <sophie.example@federation.edu.au>
2021-02-09 19:31:30 +1100

    The Main program now creates several default customers for testing with. I have also added the initial version of the Product class.

---
+++ Product.java
@@ -0,0 +1,13 @@
+
+/** This class represents products sold by the store. */
+public class Product
+{
+	private String description;
+	private double price;
+
+	public Product(String _descrip, double _price)
+	{
+		this.description = _descrip;
+		this.price = _price;
+	}
+}
